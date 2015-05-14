d6c185c142607959e52f00d159bc992371eb863b
eric_chen@xpec.com <eric_chen@xpec.com>
2015-05-15 00:19:09 +0800

    可以用matchmaking

--- Assets/LLAPITest.cs
+++ Assets/LLAPITest.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
 
 public class LLAPITest : MonoBehaviour  {
+	public Transform StartPosition;
+
+	void Awake(){
+		NetworkManager.RegisterStartPosition (StartPosition);
+	}
+
+	void Start () 
+	{
+		Utility.SetAppID ((AppID)215);
+		//NetworkMatch.SetProgramAppID((AppID)XXX);        // AppID you get when you've registered the game
+	}
 }
